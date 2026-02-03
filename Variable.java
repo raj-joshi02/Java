bd6f6a7bae12c151bdcb255dec846052f7468356
raj-joshi02 <rrajjoshi0202@gmail.com>
2026-02-04 00:40:00 +0530

    Variables in java

---
+++ Variable.java
@@ -0,0 +1,13 @@
+class Variable{
+    public static void main(String[] args){
+            int num = 3; 
+            System.out.println(num);
+            int num1 = 2;
+            int num2 = 4;
+            System.out.println(num1+num2);
+            //So, decribing num1+num2 directly in print statement we can define them in a new variable 
+            int result = num1 + num2;
+            System.out.println(result);
+    }
+
+}
